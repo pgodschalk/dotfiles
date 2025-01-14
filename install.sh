4dd8e323895df2e28afdbdc0448c2f288523d614
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-14 23:45:29 +0100

    feat: clean up existing Zsh configuration files before linking Prezto runcoms

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 git clone https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+rm -f "${ZDOTDIR:-$HOME}"/.zlogin
+rm -f "${ZDOTDIR:-$HOME}"/.zlogout
+rm -f "${ZDOTDIR:-$HOME}"/.zprofile
+rm -f "${ZDOTDIR:-$HOME}"/.zshenv
+
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zlogin "${ZDOTDIR:-$HOME}"/.zlogin
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zlogout "${ZDOTDIR:-$HOME}"/.zlogout
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zprofile "${ZDOTDIR:-$HOME}"/.zprofile
