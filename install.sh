47a842e01e6e1ac9c842f702b823357f84c410d4
Patrick Godschalk <patrick@kernelpanics.nl>
2025-01-14 23:53:42 +0100

    feat: enhance installation script to copy additional configuration files

--- install.sh
+++ install.sh
@@ -11,3 +11,26 @@ ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zlogin "${ZDOTDIR:-$HOME}"/.zlogin
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zlogout "${ZDOTDIR:-$HOME}"/.zlogout
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zprofile "${ZDOTDIR:-$HOME}"/.zprofile
 ln -s "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshenv "${ZDOTDIR:-$HOME}"/.zshenv
+
+mkdir -p "$HOME/.config"
+cp .config/starship.toml "$HOME/.config/starship.toml"
+
+mkdir -p "$HOME/Code"
+cp -R bin "$HOME/Code/bin"
+
+cp .aliases "$HOME/.aliases"
+cp .curlrc "$HOME/.curlrc"
+cp .exports "$HOME/.exports"
+cp .functions "$HOME/.functions"
+cp .gitattributes "$HOME/.gitattributes"
+cp .gitconfig "$HOME/.gitconfig"
+cp .gitignore_global "$HOME/.gitignore"
+cp .hushlogin "$HOME/.hushlogin"
+cp .inputrc "$HOME/.inputrc"
+cp .nanorc "$HOME/.nanorc"
+cp .screenrc "$HOME/.screenrc"
+cp .sources "$HOME/.sources"
+cp .wgetrc "$HOME/.wgetrc"
+cp .zmodules "$HOME/.zmodules"
+cp .zpreztorc "$HOME/.zpreztorc"
+cp .zshrc "$HOME/.zshrc"
